db8a6b23cb06e647984707e9051f01088545909b
Orest Savchak <hydr0@list.ru>
2016-08-06 04:29:00 +0300

    Add fixes for UITableView when keyboard is shown.

--- RedditClient/controller/RCLinksListViewController.m
+++ RedditClient/controller/RCLinksListViewController.m
@@ -12,7 +12,7 @@
 #import <SVPullToRefresh/UIScrollView+SVPullToRefresh.h>
 #import <SVPullToRefresh/UIScrollView+SVInfiniteScrolling.h>
 
-@interface RCLinksListViewController ()<UITableViewDataSource, RCLinksModelDelegate>
+@interface RCLinksListViewController ()<UITableViewDataSource, UITextFieldDelegate, RCLinksModelDelegate>
 
 @property (strong, nonatomic) RCLinksModel *model;
 
@@ -22,6 +22,8 @@
 @property (weak, nonatomic) IBOutlet UITextField *searchTextField;
 @property (weak, nonatomic) IBOutlet UIButton *searchButton;
 
+@property (strong, nonatomic) UIActivityIndicatorView *activityIndicator;
+
 @end
 
 @implementation RCLinksListViewController
@@ -29,6 +31,11 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     self.model.delegate = self;
+    [self addTableViewLoadingListeners];
+    [self addObserversForKeyboardHandling];
+}
+
+- (void)addTableViewLoadingListeners {
     __weak RCLinksListViewController *weakSelf = self;
     [self.linksTableView addPullToRefreshWithActionHandler:^{
         [weakSelf.linksTableView showsPullToRefresh];
@@ -40,6 +47,24 @@
     }];
 }
 
+//Careful, copy-paste!
+- (void)addObserversForKeyboardHandling {
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillShowNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        id _obj = [note.userInfo valueForKey:UIKeyboardFrameEndUserInfoKey];
+        CGRect _keyboardFrame = CGRectNull;
+        if ([_obj respondsToSelector:@selector(getValue:)]) [_obj getValue:&_keyboardFrame];
+        [UIView animateWithDuration:0.25f delay:0.f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            [_linksTableView setContentInset:UIEdgeInsetsMake(0.f, 0.f, _keyboardFrame.size.height, 0.f)];
+        } completion:nil];
+    }];
+    
+    [[NSNotificationCenter defaultCenter] addObserverForName:UIKeyboardWillHideNotification object:nil queue:nil usingBlock:^(NSNotification *note) {
+        [UIView animateWithDuration:0.25f delay:0.f options:UIViewAnimationOptionCurveEaseInOut animations:^{
+            [_linksTableView setContentInset:UIEdgeInsetsZero];
+        } completion:nil];
+    }];
+}
+
 - (RCLinksModel *)model {
     if (!_model) {
         _model = [RCLinksModel new];
@@ -47,12 +72,26 @@
     return _model;
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
+- (UIActivityIndicatorView *)activityIndicator {
+    if (!_activityIndicator) {
+        _activityIndicator = [[UIActivityIndicatorView alloc] initWithFrame:CGRectMake(0, 0, 50, 50)];
+        _activityIndicator.center = self.view.center;
+        _activityIndicator.activityIndicatorViewStyle = UIActivityIndicatorViewStyleGray;
+        [self.view addSubview:_activityIndicator];
+    }
+    return _activityIndicator;
 }
 
 - (IBAction)didClickSearch:(UIButton *)sender {
-    [self.model searchForQuery:self.searchTextField.text];
+    [self search];
+}
+
+- (void)showLoading {
+    [self.activityIndicator startAnimating];
+}
+
+- (void)hideLoading {
+    [self.activityIndicator stopAnimating];
 }
 
 - (void)didPulledToRefresh {
@@ -63,6 +102,17 @@
     [self.model updateBottomItems];
 }
 
+- (void)search {
+    [self showLoading];
+    [self clearTable];
+    [self.model searchForQuery:self.searchTextField.text];
+}
+
+- (void)clearTable {
+    self.links = nil;
+    [self.linksTableView reloadData];
+}
+
 #pragma mark RCLinksModelDelegate implementation
 
 - (void)linksModel:(RCLinksModel *)model didLinksUpdate:(NSArray<RSLink *> *)links onTop:(BOOL)onTop {
@@ -71,6 +121,7 @@
     } else {
         [self.linksTableView.infiniteScrollingView stopAnimating];
     }
+    [self hideLoading];
     
     NSIndexPath *startAtPosition = onTop ? [NSIndexPath indexPathForRow:0 inSection:0] : [self.linksTableView.indexPathsForVisibleRows.lastObject copy];
     self.links = [links copy];
@@ -85,6 +136,16 @@
 - (void)linksModel:(RCLinksModel *)model didFailedToLoadWithError:(NSError *)error {
     [self.linksTableView.pullToRefreshView stopAnimating];
     [self.linksTableView.infiniteScrollingView stopAnimating];
+}
+
+#pragma mark UITextFieldDelegate implementation
+
+- (BOOL)textFieldShouldReturn:(UITextField *)textField {
+    if (textField == self.searchTextField) {
+        [self search];
+        return YES;
+    }
+    return NO;
 }
 
 #pragma mark UITableViewDataSource implementation
